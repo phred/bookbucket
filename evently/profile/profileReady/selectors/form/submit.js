b83b390370da0836afe7730885cc87b3ed428045
Fred Alger <fred@fredalger.net>
2010-07-14 10:58:07 -0400

    Initial commit.

---
+++ evently/profile/profileReady/selectors/form/submit.js
@@ -0,0 +1,16 @@
+function() {
+  var form = this;
+  //var doc = {
+  //  created_at : new Date(),
+  //  profile : $$("#profile").profile,
+  //  title : $("[name=title]", form).val(),
+  //  author : $("[name=author]", form).val(),
+  //  publication_date : $("[name=publication_date]", form).val()
+  //};
+  //$$(this).app.db.saveDoc(doc, {
+  //  success : function() {
+  //    $("[type=text]", form).val("");
+  //  }
+  //});
+  return false;
+};
