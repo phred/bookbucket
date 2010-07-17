83e47c451765ba6a0cb2a215d227f4b6d64b6009
Fred Alger <fred@fredalger.net>
2010-07-17 01:11:25 -0400

    Fix built in documentation

--- vendor/couchapp/evently/docs/topic/data.js
+++ vendor/couchapp/evently/docs/topic/data.js
@@ -1,6 +1,6 @@
 function(e, p) {
   var doc = $$(this).app.ddoc.vendor.couchapp.docs[p.id];
-  var converter = new Showdown.converter();
+  var converter = $$(this).app.require('vendor/couchapp/lib/markdown');
   var html = converter.makeHtml(doc);
   return {
     html : html
